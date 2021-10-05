89a479095c3659ad2e8dec9627dfebe8e97f3dc0
Alex Zhou <alex.lawsonzhou@hotmail.com>
2021-10-04 23:14:02 -0400

    relation network initial commit

--- train_omniglot_few_shot.py
+++ train_omniglot_few_shot.py
@@ -44,5 +44,35 @@ class CNNEncoder(nn.Module):
 
 
 class RelationNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size, hidden_size):
         super(RelationNetwork, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(128, 64, kernel_size=3, padding=1), # why is in_channel 128? because after feature concatenation.
+            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64, momentum=1, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        return out
+
+
+def weights_init(m):
+    class_name = m.__class__.__name__
+
+
+def main():
+    
